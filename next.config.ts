246d6adf07e6c02c5ec16dd318d22b7b31242586
faseeha-jamal <jaseehajamal08@gmail.com>
2026-06-23 10:31:54 +0530

    fix: update baseline-browser-mapping version to 2.10.37 and remove experimental appDir option

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
-  experimental: {
-    appDir: true,
-  },
-  /* config options here */
   reactCompiler: true,
-
 };
 
 export default nextConfig;
